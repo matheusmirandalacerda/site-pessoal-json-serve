aa651dccb73421706132505569965b4ca57e3857
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2023-05-22 11:39:49 -0300

    Update Contato.jsx

--- src/componentes/Contato/Contato.jsx
+++ src/componentes/Contato/Contato.jsx
@@ -37,7 +37,8 @@ function Contato() {
 
 
     return (
-        <form onSubmit={enviarFormulario}>
+        <main>
+          <form onSubmit={enviarFormulario}>
 
             <fieldset>
                 <label htmlFor="input-nome">Nome</label>
@@ -96,7 +97,19 @@ function Contato() {
             </center>
 
         </form>
-    )
+
+      <section> 
+    <h2>Contato</h2>
+    <p>Telefone: (81) 9 9999-0763<br></br>
+    E-mail: matheus.miranda@hotmail.com<br></br>
+    Github: <a target = "_blank" href = "https://github.com/matheusmirandalacerda">github.com/matheusmirandalacerda</a><br></br>
+    LINKEDIN: <a target = "_blank" href = "https://www.linkedin.com/in/matheus-miranda-lacerda-93b7a61b7/">linkedin.com/in/matheus-miranda-lacerda-93b7a61b7</a></p>
+    </section>
+    </main>
+
+    
+  );
+
 }
 
 export default Contato;
