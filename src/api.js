d3c792bc7265b88a3b9fe14d533542b833e1bc57
Matheus <108967989+matheusmirandalacerda@users.noreply.github.com>
2023-05-30 16:58:49 -0300

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'https://json-server-ex-gq2u.onrender.com',
     //baseURL: 'http://localhost:5000',
+    //baseURL: 'https://site-pessoal-api-0f6p.onrender.com'
 });
 
 export default api;
